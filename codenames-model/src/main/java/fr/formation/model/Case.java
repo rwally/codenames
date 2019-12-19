d97cb68bbb34404a81aa0435381b14f328444a30
Reaz Wally <reaz.wally@soprasteria.com>
2019-12-19 16:57:21 +0100

    Update model Reaz

--- codenames-model/src/main/java/fr/formation/model/Case.java
+++ codenames-model/src/main/java/fr/formation/model/Case.java
@@ -6,15 +6,38 @@ public class Case {
 	
 	private Couleur couleur;
 	private Mot mot;
-	//private Grille grille;
+	private Grille grille;
+	private boolean trouver=false;
 	
 	public Case() {		
 	}
 
-	public Case(Couleur couleur, Mot mot) {
+	public Case(Couleur couleur, Mot mot, Grille grille) {
 		this.couleur = couleur;
 		this.mot = mot;
+		this.grille = grille;
 	}
+	
+	public boolean isTrouver() {
+		return trouver;
+	}
+	
+	public void setTrouver(boolean trouver) {
+		this.trouver = trouver;
+	}
+
+
+
+	public Grille getGrille() {
+		return grille;
+	}
+
+
+
+	public void setGrille(Grille grille) {
+		this.grille = grille;
+	}
+
 
 	public Couleur getCouleur() {
 		return couleur;
@@ -32,55 +55,56 @@ public class Case {
 		this.mot = mot;
 	}
 	
-	public ArrayList<Case> creerListeCases(ArrayList<Mot> listeMots){
+	public ArrayList<Case> creerListeCases(ArrayList<Mot> listeMots, Grille maGrille){
 		
 		ArrayList<Case> cases = new ArrayList<Case>();
+		int taille = 0, r = 0,b = 0,w = 0; //rouge bleu w=blanc
 		
-		for(int i = 0; i < 25; i++) {
+		switch(maGrille.getDifficulte().getValeur()) {
+			case 4: taille=16;
+					r=6;
+					b=11;
+					w=16;
+					break;
+					
+			case 5: taille=25;
+					r=9;
+					b=17;
+					w=24;
+					break;
+					
+			case 6 :taille=36;
+					r=12;
+					b=23;
+					w=33;
+					break;
+		} 
+		
+		
+		for(int i = 0; i < taille; i++) {
 				Case maCase = new Case();
 				maCase.setMot(listeMots.get(0));
 				listeMots.remove(0);
-				if(i<9) {
+				if(i<r) {
 					maCase.setCouleur(Couleur.rouge);
-				}else if(i<17) {
+				}else if(i<b) {
 					maCase.setCouleur(Couleur.bleu);
-				}else if(i<24) {
+				}else if(i<w) {
 					maCase.setCouleur(Couleur.blanc);
 				}else {
 					maCase.setCouleur(Couleur.noir);
 				}			
+		
 				cases.add(maCase);
 		}
 		
+			
 		return cases;
 		
 	}
 
 	
-	/*
-	for(int i = 0; i < 9; i++) {
-		Case maCase = new Case();
-		maCase.setMonMot(myResult.getString("mot"));
-		maCase.setMaCouleur(Couleur.rouge);
-		cases.add(maCase);
-	}
-	for(int i = 0; i < 8; i++) {
-		Case maCase = new Case();
-		maCase.setMonMot(myResult.getString("mot"));
-		maCase.setMaCouleur(Couleur.bleu);
-		cases.add(maCase);
-	}
-	for(int i = 0; i < 7; i++) {
-		Case maCase = new Case();
-		maCase.setMonMot(myResult.getString("mot"));
-		maCase.setMaCouleur(Couleur.blanc);
-		cases.add(maCase);
-	}
-	Case maCase = new Case();
-	maCase.setMonMot(myResult.getString("mot"));
-	maCase.setMaCouleur(Couleur.noir);
-	cases.add(maCase);
-	*/
+	
 }
 
 
--- codenames-model/src/main/java/fr/formation/model/Participation.java
+++ codenames-model/src/main/java/fr/formation/model/Participation.java
@@ -1,6 +1,32 @@
 package fr.formation.model;
 
 public class Participation {
+	
 	private Role role;
 	private Joueur joueur;
+	
+	public Participation(Role role, Joueur joueur) {
+		this.role = role;
+		this.joueur = joueur;
+	}
+
+	public Participation() {
+		
+	}
+	
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}
 }
