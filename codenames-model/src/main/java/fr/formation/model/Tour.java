5d45e416a600ae9111cc2f1bb3c97f50d38d3605
adeline <adeline.sinapayen@soprasteria.com>
2019-12-27 10:31:04 +0100

    Votes

--- codenames-model/src/main/java/fr/formation/model/Tour.java
+++ codenames-model/src/main/java/fr/formation/model/Tour.java
@@ -1,7 +1,9 @@
 package fr.formation.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import javax.persistence.Column;
@@ -11,7 +13,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "tour")
 public class Tour {
@@ -19,18 +20,8 @@ public class Tour {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
-	//private List<Mot> CodeNames = new ArrayList<Mot>();
+	// private List<Mot> CodeNames = new ArrayList<Mot>();
 
-//	public Case voteGlobal(List<Case> votes) {
-//		Case maxCase = null;
-//		int maxCount = 0;
-//				
-//		for (Case c : votes) {
-//			maxCase = c;
-//		}
-//		return maxCase;
-//	}
-	
 
 	public int getId() {
 		return id;
@@ -40,6 +31,30 @@ public class Tour {
 		this.id = id;
 	}
 
+//	/!\ méthode non testée
+	public Case voteGlobal(List<Case> votes) {
+		Map<Case, Integer> map = new HashMap<Case, Integer>();
+		int max = 0;
+		Case laCase = null;
+
+		for (int i = 0; i < votes.size(); i++) {
+			int value = 1;
+			Case key = votes.get(i);
+
+			if (map.containsKey(key)) {
+				value = map.get(key);
+			}
+			map.put(key, value);
+
+			if (value > max) {
+				max = value;
+				laCase = key;
+			}
+		}
+		return laCase;
+	}
+
+//	/!\ méthode non testée
 	public Case voteJoueur(Grille grille) {
 		Case maCase = new Case();
 		maCase = null;
@@ -51,12 +66,12 @@ public class Tour {
 			String mot = sc.nextLine();
 
 			for (Case c : grille.getCases()) {
-				
-					if (c.getMot().getLibelle().equals(mot)) {
-						maCase = c;
-						saisieOK = true;
-					}
-				
+
+				if (c.getMot().getLibelle().equals(mot)) {
+					maCase = c;
+					saisieOK = true;
+				}
+
 			}
 			if (!saisieOK) {
 				System.out.println("Veuillez écrire un mot de la liste. Respectez la casse");
@@ -64,70 +79,63 @@ public class Tour {
 		}
 		return maCase;
 	}
-	
-	
-	
-	
-	
-	public boolean CodeName (Joueur j) {
+
+	public boolean CodeName(Joueur j) {
 		String code = null;
 		int nbMots = -1;
-		
+
 		boolean valid = false;
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		if(j.getRole() == Role.master) {
-			
-			
-			 System.out.println("Entrez le nom de code :");
-			 code = sc.nextLine();
-			 System.out.println("Entrez un nombre de mots à deviner :");
-			 nbMots = sc.nextInt();
-			 
-			 System.out.println("Choisissez les mots à faire deviner :");
-			 
-			 switch (nbMots) {
-				
-				case 0:
-					
-				case 1:
-					System.out.println("Choisissez le mot à deviner dans la grille :");
-					String codeName = sc.nextLine();
-					
-				case 2:
-					System.out.println("Entrez les deux noms de code :");
-					String codeName1 = sc.nextLine();
-					String codeName2 = sc.nextLine();
-					
-					if(codeName1.equals(code)) {
-						valid = true;
-					}
-				}
-			 
-			 
-		}
-		else {
-			System.out.println(code + " en " + nbMots + " mots.");
-			
+
+		if (j.getRole() == Role.master) {
+
+			System.out.println("Entrez le nom de code :");
+			code = sc.nextLine();
+			System.out.println("Entrez un nombre de mots à deviner :");
+			nbMots = sc.nextInt();
+
+			System.out.println("Choisissez les mots à faire deviner :");
+
 			switch (nbMots) {
-			
+
 			case 0:
-				
+
 			case 1:
-				System.out.println("Entrez le nom de code :");
+				System.out.println("Choisissez le mot à deviner dans la grille :");
 				String codeName = sc.nextLine();
-				
-				if(codeName.equals(code)) {
-					valid = true;
-				}
-				
+
 			case 2:
 				System.out.println("Entrez les deux noms de code :");
 				String codeName1 = sc.nextLine();
 				String codeName2 = sc.nextLine();
-				
-				if(codeName1.equals(code)) {
+
+				if (codeName1.equals(code)) {
+					valid = true;
+				}
+			}
+
+		} else {
+			System.out.println(code + " en " + nbMots + " mots.");
+
+			switch (nbMots) {
+
+			case 0:
+
+			case 1:
+				System.out.println("Entrez le nom de code :");
+				String codeName = sc.nextLine();
+
+				if (codeName.equals(code)) {
+					valid = true;
+				}
+
+			case 2:
+				System.out.println("Entrez les deux noms de code :");
+				String codeName1 = sc.nextLine();
+				String codeName2 = sc.nextLine();
+
+				if (codeName1.equals(code)) {
 					valid = true;
 				}
 			}
