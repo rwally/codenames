4c7417a58110bf0f9e3eaa1dfdf18a5facbf20d2
adeline <adeline.sinapayen@soprasteria.com>
2020-01-03 09:33:30 +0100

    Changement place JpaConfig

--- codenames-data-jpa/src/test/java/fr/formation/test/TestDAO.java
+++ codenames-data-jpa/src/test/java/fr/formation/test/TestDAO.java
@@ -18,6 +18,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import fr.formation.config.JpaConfig;
 import fr.formation.dao.IDAOCase;
 import fr.formation.dao.IDAOMot;
 import fr.formation.dao.IDAOUtilisateur;
--- codenames-java/src/test/java/fr/formation/service/MenuTest.java
+++ codenames-java/src/test/java/fr/formation/service/MenuTest.java
@@ -1,4 +1,8 @@
 package fr.formation.service;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class MenuTest {
 }
